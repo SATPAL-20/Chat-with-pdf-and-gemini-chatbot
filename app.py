56f7bb815699f2adb80a9684ed32741bd5816772
SATPAL-20 <131527626+SATPAL-20@users.noreply.github.com>
2024-05-10 01:50:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,190 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+import google.generativeai as gen_ai
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+
+# Load environment variables
+load_dotenv()
+
+# Configure Streamlit page settings
+st.set_page_config(
+    page_title="Chat with Gemini-Pro!",
+    page_icon=":brain:",  # Favicon emoji
+    layout="wide"  # Page layout option
+)
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+# Set up Google Gemini-Pro AI model
+gen_ai.configure(api_key=GOOGLE_API_KEY)
+model = gen_ai.GenerativeModel('gemini-pro')
+
+
+# Function to translate roles between Gemini-Pro and Streamlit terminology
+def translate_role_for_streamlit(user_role):
+    if user_role == "model":
+        return "assistant"
+    else:
+        return user_role
+
+
+# Function to clear chat history
+def clear_chat_history():
+    session_state.chat_session = model.start_chat(history=[])
+
+
+# Function to read all PDF files and return text
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+# Function to split text into chunks
+def get_text_chunks(text):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=10000, chunk_overlap=1000)
+    chunks = splitter.split_text(text)
+    return chunks  # list of strings
+
+
+# Function to get embeddings for each chunk
+def get_vector_store(chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001")  # type: ignore
+    vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+# Function to get conversational chain
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                                   client=gen_ai,
+                                   temperature=0.3,
+                                   )
+    prompt = PromptTemplate(template=prompt_template,
+                            input_variables=["context", "question"])
+    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
+    return chain
+
+
+# Function to handle user input for PDF chat
+def pdf_user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001")  # type: ignore
+
+    new_db = FAISS.load_local(
+        "faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents": docs, "question": user_question}, return_only_outputs=True, )
+
+    return response
+
+
+# Initialize chat session
+class SessionState:
+    def __init__(self):
+        self.chat_session = None
+
+
+session_state = SessionState()
+
+# Main page
+def main():
+    st.title("Chat with Gemini-Pro!")
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", ("Chat with PDF Files", "Chat with Gemini-Pro"))
+
+    if page == "Chat with PDF Files":
+        st.title("Chat with PDF Files")
+        with st.expander("Upload PDF Files"):
+            pdf_docs = st.file_uploader(
+                "Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+            if st.button("Submit & Process"):
+                with st.spinner("Processing..."):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    get_vector_store(text_chunks)
+                    st.success("Done")
+
+        st.title("Chat with PDF Files")
+        st.write("Welcome to the chat!")
+        st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+        if session_state.chat_session is None:
+            session_state.chat_session = model.start_chat(history=[])
+            st.session_state.chat_session = session_state.chat_session
+
+        if "messages" not in st.session_state.keys():
+            st.session_state.messages = [
+                {"role": "assistant", "content": "Upload some PDFs and ask me a question"}]
+
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
+
+        if prompt := st.chat_input():
+            st.session_state.messages.append(
+                {"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.write(prompt)
+
+        if st.session_state.messages[-1]["role"] != "assistant":
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    response = pdf_user_input(prompt)
+                    full_response = ''
+                    for item in response['output_text']:
+                        full_response += item
+                    st.markdown(full_response)
+            if response is not None:
+                message = {"role": "assistant", "content": full_response}
+                st.session_state.messages.append(message)
+
+    else:
+        st.title("🤖 Gemini Pro - ChatBot")
+        # Display the chat history
+        for message in st.session_state.chat_session.history:
+            with st.chat_message(translate_role_for_streamlit(message.role)):
+                st.markdown(message.parts[0].text)
+
+        # Input field for user's message
+        user_prompt = st.chat_input("Ask Gemini-Pro...")
+        if user_prompt:
+            # Add user's message to chat and display it
+            st.chat_message("user").markdown(user_prompt)
+
+            # Send user's message to Gemini-Pro and get the response
+            gemini_response = st.session_state.chat_session.send_message(
+                user_prompt)
+
+            # Display Gemini-Pro's response
+            with st.chat_message("assistant"):
+                st.markdown(gemini_response.text)
+
+
+if __name__ == "__main__":
+    main()
